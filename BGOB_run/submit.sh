0ed204074e0664859b4be4dfd019e73a903a953d
Ruiqi Lyu <64272391+Rachel-Lyu@users.noreply.github.com>
2023-09-21 13:11:19 -0400

    Update submit.sh

--- BGOB_run/submit.sh
+++ BGOB_run/submit.sh
@@ -2,4 +2,4 @@
 #SBATCH --job-name=BGOB
 source ~/.bashrc
 conda activate BGOB
-python main.py --model_name ECC_90 --dataset ~/BGOB_run/demo.csv --delta_t 1 --hidden_size 300 --p_hidden 600 --mixing 0.0001 --epoch 300 --whole_dataset
+python main.py --model_name ECC_90 --dataset demo.csv --delta_t 1 --hidden_size 300 --p_hidden 600 --mixing 0.0001 --epoch 300 --whole_dataset
